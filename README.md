72509b90e30df0d195a5d9193be2e0328c5a110f
Scott <67354101+scGetStuff@users.noreply.github.com>
2022-09-18 17:06:05 -0500

    first draft code for API call

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # top-js-weather-app
 The Odin Project, Full Stack JavaScript Path, JavaScript, JavaScript in the Real World, Project: Weather App
+
+From spec
+- search for a specific location
+- parse useful info out of API result
+- toggle displaying the data in Fahrenheit or Celsius
+- change the look of the page based on the data
+- Optional: add a ‘loading’ component that displays while asynch code runs
+
+
+API docs
+https://openweathermap.org/current#name
+`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
+`https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}`
+`https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}`
--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
 "use strict";
 
-import styles from './styles.css';
+import styles from "./styles.css";
 
-function component() {
-    const element = document.createElement("div");
-    element.innerText = "test";
-    return element;
+const cl = console.log;
+
+async function doStuffAwait(city) {
+    console.clear();
+
+    const key = "546c35a9a1c0387037c25c3d76b013fc";
+    const uri = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    const searchResults = document.createElement("div");
+    const main = document.querySelector("main");
+
+    while (main.firstChild) main.removeChild(main.firstChild);
+    
+    const response = await fetch(uri, { mode: "cors" });
+    if (response.status != 200) {
+        searchResults.innerText = response.status;
+        return;
+    }
+    const data = await response.json();
+    searchResults.innerText = JSON.stringify(data);
+    main.appendChild(searchResults);
 }
 
-// document.body.appendChild(component());
+function doStuff(city) {
+    console.clear();
+
+    const key = "546c35a9a1c0387037c25c3d76b013fc";
+    const uri = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    const searchResults = document.createElement("div");
+    const main = document.querySelector("main");
+
+    while (main.firstChild) main.removeChild(main.firstChild);
+
+    fetch(uri, { mode: "cors" })
+        .then((response) => {
+            if (response.status != 200) throw new Error(response.status);
+            return response.json();
+        })
+        .then((data) => {
+            cl(data);
+            searchResults.innerText = JSON.stringify(data.main);
+        })
+        .catch(alert);
+    main.appendChild(searchResults);
+}
+
+doStuffAwait("London");
+//doStuff("London");
